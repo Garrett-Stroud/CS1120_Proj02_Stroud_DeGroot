6b275001725f8a7a8d214adfd96afe1ce6d1570d
gracedegroot <grace.degroot@gmail.com>
2026-03-04 10:04:35 -0500

    Import BoggleBoard class in main.py
    Added import statement for BoggleBoard class.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 #Description: Main function that allows the user to enter a seed and word, and generates them a boggle board based on the seed.
 #The user can enter a desired word, and it is checked for correctness. It also checks for if the word is a palindrone.
 
-
+#Imports the file with the board class.
 import BoggleBoard
 
 def main():
