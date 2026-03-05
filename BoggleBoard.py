32416657d3409a030c7f03ec30aafd385183527b
Garrett Stroud <gcf0459@wmich.edu>
2026-03-04 23:05:40 -0500

    Update BoggleBoard.py

--- BoggleBoard.py
+++ BoggleBoard.py
@@ -1,24 +1,23 @@
-# You should have a class called BoggleBoard, which includes the two-dimensional lists of letters,
-# methods for filling and printing it, and a method that returns whether or not a given word is valid
-# for the board. This last method should make use of another recursive method for verifying
-# words.
+# Project No.2#
+# Authors: Grace DeGroot and Garrett Stroud
 import random
 import string
 
 
+# Class representing a BoggleBoard
 class BoggleBoard:
-    def __init__(self,seed = None):
+    def __init__(self, seed = None):
         self.board = []
-        if seed is not None:
+        if seed is not None: # Sets random seed if specified
             random.seed(seed)
 
-    def get_seed(self,seed):
-        return random.seed(seed)
 
+    # Method that randomly selects a letter from the alphabet
     def create_dice(self):
         dice = random.choice(string.ascii_uppercase)
         return dice
 
+    # Method to generate board
     def fill_board(self):
         for row in range(4):
             row_vals = []
@@ -27,59 +26,50 @@ class BoggleBoard:
             self.board.append(row_vals)
         return self.board
 
-
+    # Method to print board
     def print_board(self):
 
-        border = '+---+ +---+ +---+ +---+'
-
-        for row in range(4):
-            print(border)
-            for col in range(4):
-                print(f'| {self.board[row][col]} |', end=" ")
-            print()
-            print(border)
-
-    def print_highlighted(self, path):
         border = '+---+---+---+---+'
 
         for row in range(4):
             print(border)
             for col in range(4):
+                print(f'| {self.board[row][col]} ', end="")
+            print("|")
+            print(border)
 
-                if (row, col) in path:
-                    print(f'|<{self.board[row][col]}>|', end="")
+    # Method that prints board and highlights word
+    def print_highlighted(self, path):
+        print()
+        border = '+---+---+---+---+'
+        for row in range(4):
+            print(border)
+
+            for col in range(4):
+                if (row,col)  in path:
+                    print(f'|<{self.board[row][col]}>', end="")
                 else:
-                    print(f'| {self.board[row][col]} |', end="")
+                    print(f'| {self.board[row][col]} ', end="")
 
-            print()
-
-        print(border)
-
+            print("|")
+            print(border)
 
 
-
-
-
-
-
-
+    # method to check users word guess
     def check_board(self, word):
         # find initial position and run recursion method from there
-
+        word = word.upper()
         for row in range(4):
             for col in range(4):
                 if self.board[row][col].upper() == word[0]:
                     path = []
                     if self.boggle_validation(word, 0, row, col,path):
                         return path
-
+        
         return False
 
-
-
-
-
-    def boggle_validation(self,word,index,row,col,path):
+    # Recursive method that checks possible paths for the word
+    def boggle_validation(self, word, index, row, col, path):
         # Base case
         if index == len(word):
             return True
@@ -91,34 +81,31 @@ class BoggleBoard:
         if (row, col) in path:
             return False
 
-        path.append((row, col))
-
 
         # Letter validation
         if self.board[row][col] != word[index]:
             return False
+        
+        path.append((row, col))
 
 
-
-
-
-        # Logic to check values around a box
+        # Directions representing adjacent tiles
         directions = [
-            [0, -1],  # left
+        [0, -1],  # left
         [0, 1], # right
         [-1, 0], # above
         [1, 0] # below
         ]
 
         # Loops through every direction
-        for r,c in directions:
+        for r, c in directions:
             new_row = row + r
             new_col = col + c
 
             # Recursion method
-            if self.boggle_validation(word,index+1,new_row,new_col,path):
+            if self.boggle_validation(word, index + 1, new_row, new_col, path):
                 return True
 
-        # Returns False when all fails
+        # Returns False when all fails and remove last element
         path.pop()
         return False
